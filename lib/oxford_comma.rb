e3b70f2349eb1344d5888af7304044320793fd6d
Celeste Gerard <celeste.gerard@gmail.com>
2018-03-04 14:10:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+ if array.size < 2
+    array.join()
+    elsif array.size == 2 
+  newArray = []
+  newArray << array.insert(1, " and ")
+  newArray.join
+  else
+  newArray = []
+  last_word = array.pop()
+  array.each do |word|
+    newArray.push("#{word}, ")
+  end
+  newArray.insert(-1, last_word)
+  newArray.insert(-2, "and ")
+  newArray.join
+  end
 end
+
+
